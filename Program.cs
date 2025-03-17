5520d263c1a370665baac37f8caa704ed0540df2
Danilo Costa <dandan_costa2222@outlook.com>
2025-03-16 22:18:01 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int numero = 0;
+
+Console.Write("Insira um numero inteiro: ");
+numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"O antecessor do {numero} é {numero - 1}");
+Console.WriteLine($"O sucessor do {numero} é {numero + 1}");
+
+
+
+
